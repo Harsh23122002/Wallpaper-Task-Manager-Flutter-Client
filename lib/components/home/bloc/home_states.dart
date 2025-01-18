be5723aad81fc3c4cc2f9428b950b6e6e952d074
harsh <harshgwaghmare@gmail.com>
2025-01-18 08:08:21 +0530

    feat: home bloc configured

--- lib/components/home/bloc/home_states.dart
+++ lib/components/home/bloc/home_states.dart
@@ -1,1 +1,9 @@
+abstract class HomeState {}
 
+class HomeInitialState extends HomeState {}
+
+class HomeLoadingState extends HomeState {}
+
+class HomeAvailableState extends HomeState {}
+
+class HomeErrorState extends HomeState {}
